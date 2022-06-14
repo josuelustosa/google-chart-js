6dff632aea7d3588f14ee4e1762d575bb31b8f86
Josué <josue.filho@conecthus.org.br>
2022-06-14 16:57:26 -0400

    :construction: custom bar chart with json

--- jsonChart.js
+++ jsonChart.js
@@ -3,14 +3,38 @@ function drawJsonChart() {
   var dadosJson = $.ajax({
     url: "dados.json",
     dataType: "json",
-    async: false
+    async: false,
   }).responseText;
 
   var table = new google.visualization.DataTable(dadosJson);
+
+  table.sort([
+    {
+      column: 1, 
+      desc: false
+    }
+  ]);
+
+  var options = {
+    title: "Usuários e Poupanças",
+    weidht: 920,
+    height: 600,
+    hAxis: {
+      gridlines: {
+        color: "transparent",
+      },
+      format: "currency",
+      textPosition: "none"
+    },
+    annotations: {
+      alwaysOutside: true,
+    },
+    legend: "none",
+  };
 
   var chart = new google.visualization.BarChart(
     document.getElementById("myBarChartWithJson")
   );
 
-  chart.draw(table);
+  chart.draw(table, options);
 }
