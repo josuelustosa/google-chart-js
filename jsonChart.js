21f493fdf4639dfd9a13bbb35800db4ae8d2edce
Josué <josue.filho@conecthus.org.br>
2022-06-14 17:06:45 -0400

    :construction: add json file of guist

--- jsonChart.js
+++ jsonChart.js
@@ -1,7 +1,7 @@
 function drawJsonChart() {
   // BAR CHART WITH JSON
   var dadosJson = $.ajax({
-    url: "dados.json",
+    url: "https://gist.githubusercontent.com/josuelustosadev/ba5bf23ea7f4852088f25fdaed1975cc/raw/8b2b08ce028299cb980d2ff5ba7d8a039b9f51d3/dados.json",
     dataType: "json",
     async: false,
   }).responseText;
